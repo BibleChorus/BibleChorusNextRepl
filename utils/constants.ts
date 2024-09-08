97e985fd108bc76dcb3091f8b8630fc19d27c1b5
BibleChorus <admin@biblechorus.com>
2024-09-08 04:29:28 +0000

    9-7-24-929-constants

---
+++ utils/constants.ts
@@ -0,0 +1,71 @@
+export const BIBLE_BOOKS = [
+  'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
+  'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel',
+  '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles', 'Ezra',
+  'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs',
+  'Ecclesiastes', 'Song of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations',
+  'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos',
+  'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk',
+  'Zephaniah', 'Haggai', 'Zechariah', 'Malachi',
+  'Matthew', 'Mark', 'Luke', 'John', 'Acts',
+  'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians',
+  'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy',
+  '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James',
+  '1 Peter', '2 Peter', '1 John', '2 John', '3 John',
+  'Jude', 'Revelation'
+];
+
+export const VERSE_COUNTS = {
+  total: 31102,
+  oldTestament: 23145,
+  newTestament: 7957,
+};
+
+export const GENRES = [
+  'Worship', 'Hymn', 'Contemporary', 'Choral',
+  'Rock', 'Pop', 'Hip Hop/Rap', 'R&B/Soul', 'Country',
+  'Jazz', 'Blues', 'Classical', 'Electronic/Dance', 'Folk',
+  'Reggae', 'Metal', 'Punk', 'Indie', 'Alternative',
+  'Funk', 'Disco', 'World Music', 'Latin', 'Ska',
+  'Grunge', 'Techno', 'House', 'Ambient', 'Trance',
+  'Dubstep', 'Trap', 'Experimental', 'Bluegrass', 'Opera',
+  'Symphonic', 'A Cappella', 'Baroque', 'Bossa Nova', 'Samba', 'Middle Eastern'
+];
+
+export const INSTRUMENTS = [
+  'Piano', 'Guitar', 'Drums', 'Bass', 'Violin',
+  'Cello', 'Flute', 'Trumpet', 'Saxophone', 'Organ',
+  'Synthesizer', 'Harp', 'Accordion', 'Harmonica', 'Ukulele',
+  'Tambourine', 'Maracas', 'Congas', 'Djembe', 'Steel Drums',
+  'Bagpipes'
+];
+
+export const MOODS = [
+  'Joyful', 'Reflective', 'Uplifting', 'Peaceful', 'Energetic',
+  'Somber', 'Celebratory', 'Contemplative', 'Hopeful', 'Grateful',
+  'Melancholy', 'Sentimental', 'Tender', 'Triumphant', 'Wistful',
+  'Ethereal', 'Mysterious', 'Enchanted', 'Whimsical', 'Playful',
+  'Calm', 'Meditative', 'Tranquil', 'Intense', 'Passionate',
+  'Melodic', 'Harmonious', 'Rhythmic', 'Dynamic', 'Vibrant',
+  'Silent', 'Noisy', 'Lyrical', 'Narrative', 'Dramatic'
+];
+
+export const THEMES = [
+  'Praise', 'Worship', 'Thanksgiving', 'Repentance', 'Salvation',
+  'Faith', 'Hope', 'Love', 'Grace', 'Forgiveness',
+  'Healing', 'Comfort', 'Guidance', 'Discipleship', 'Mission',
+  'Creation', 'Redemption', 'Covenant', 'Judgment', 'Consolation',
+  'Courage', 'Wisdom', 'Glory', 'Kingdom', 'Revelation',
+  'Redeemer', 'Restoration', 'Justice', 'Peace', 'Prosperity',
+  'Knowledge'
+];
+
+export const AI_MUSIC_MODELS = [
+  'Suno 3.5',
+  'Udio 1.5'
+];
+
+export const AI_VIDEO_MODELS = [
+  'Runway Gen3',
+  'Sora'
+];
